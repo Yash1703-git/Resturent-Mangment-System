9a04da9b2e50144d20c233305aa2e1c0e209e7f9
Yash1703-git <kumbharyash766@gmail.com>
2026-02-05 22:17:39 +0530

    SMALL ISSUE

--- client/src/Components/DishCard.jsx
+++ client/src/Components/DishCard.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import CartContext from '../contexts/cartContext';
+import CartContext from '../contexts/CartContext';
 import AuthContext from '../contexts/AuthContext';
 
 export default function DishCard({ food }) {
